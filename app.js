506edd2fdce16d120ff583bcf170f90b196bcedf
Alan Kelly <alankelly@gmail.com>
2022-02-06 16:41:31 +0000

    added function to remove some repetitive code

--- app.js
+++ app.js
@@ -5,30 +5,32 @@ const stopBtn = document.getElementById("btn-stop")
 const waitBtn = document.getElementById("btn-wait")
 const goBtn = document.getElementById("btn-go")
 
-stopBtn.addEventListener("click", function(){
-    red.style.background = "rgba(255,0,0)"
-    red.style.boxShadow = "0px 0px 50px 20px rgb(255,0,0)"
+function lightsOff(){
+    //clear red light
+    red.style.background = ("rgba(255, 0, 0, 0.5)")
+    red.style.boxShadow = ""
+    //clear amber light
     amber.style.background = ("rgb(240, 163, 10)")
     amber.style.boxShadow = ""
-    green.style.background = ("rgba(0, 255, 0, 0.2)")
+    // clear green light
     green.style.boxShadow = ""
+    green.style.background = ("rgba(0, 255, 0, 0.2)")
+}
+
+stopBtn.addEventListener("click", function(){
+    lightsOff()
+    red.style.background = "rgba(255,0,0)"
+    red.style.boxShadow = "0px 0px 50px 20px rgb(255,0,0)"
 })
 
 waitBtn.addEventListener("click", function(){
-    red.style.background = ("rgba(255, 0, 0, 0.5)")
-    red.style.boxShadow = ""
-    green.style.background = ("rgba(0, 255, 0, 0.2)")
-    green.style.boxShadow = ""
+    lightsOff()
     amber.style.background = "rgb(240, 163, 10)"
     amber.style.boxShadow = "0px 0px 50px 20px rgb(240, 163, 10)"
 })
 
 goBtn.addEventListener("click", function(){
+    lightsOff()
     green.style.background = "rgba(0,255,0)"
     green.style.boxShadow = "0px 0px 50px 20px rgb(0,255,0)"
-    red.style.background = ("rgba(255, 0, 0, 0.5)")
-    red.style.boxShadow = ""
-    amber.style.background = ("rgb(240, 163, 10)")
-    amber.style.boxShadow = ""
-
 })
